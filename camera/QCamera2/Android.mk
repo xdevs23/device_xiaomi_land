c8fa605d80a938276d6269dc3f28737927fc20d6
Simao Gomes Viana <xdevs23@outlook.com>
2018-01-13 23:42:00 +0100

    yum yum

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -99,7 +99,7 @@ endif
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        hardware/qcom/display/libqservice
+        $(call project-path-for,qcom-display)/libqservice
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl libsync libgui
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
