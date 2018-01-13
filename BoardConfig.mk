eb49d8a15cbea62c0ebee8e63a6e51f54b534c84
Simao Gomes Viana <xdevs23@outlook.com>
2018-01-13 18:02:09 +0100

    AOSP HAAAALS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,8 +201,8 @@ TARGET_POWERHAL_VARIANT := qcom
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
 # Qualcomm
-BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
+BUILD_CAF_DATASERVICES := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/recovery/fstab.qcom
